69f9e0b18112a7111c8b9e8a227993424547a362
Friies <bahram.parsa2019@gmail.com>
2023-11-26 22:40:16 -0500

    adjust css flexibility between devices.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div
       name="about"
-      className="bg-gradient-to-b from-gray-800 to-black text-white w-full h-screen"
+      className="bg-gradient-to-b from-gray-800 to-black text-white w-full min-h-screen"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="pb-8">
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       name="contact"
       className="w-full h-screen bg-gradient-to-b from-black to-gray-800 p-4 text-white py-8"
     >
-      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
+      <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto min-h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Contact
